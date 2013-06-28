386b0648511aa2acb7d49d767a9df4a48e17b52a
Simon <simon.dujancourt.etu@univ-lemans.fr>
2013-06-28 20:25:28 +0200

    [Identification] Mise en forme
    + Petit effet CSS3 lorsqu'un champ est survolé
    + Ajout des balises HTML5 required pour le nom d'utilisateur et le mot
    de passe.

--- app/views/security/login.php
+++ app/views/security/login.php
@@ -8,7 +8,7 @@
     <tr class="errors">
         <td colspan="2">
             <ul>
-                <?php foreach ( $errors as $error ): ?>
+                <?php foreach ($errors as $error): ?>
                 <li><?php echo $error ?></li>
                 <?php endforeach ?>
             </ul>
@@ -17,11 +17,11 @@
     <?php endif ?>
     <tr>
         <td><label for="username">Nom d'utilisateur:</label></td>
-        <td><input type="text" name="username" placeholder="Nom d'utilisateur" value="<?php echo isset($username) ? $username : '' ?>" /></td>
+        <td><input type="text" name="username" placeholder="Nom d'utilisateur" required /></td>
     </tr>
     <tr>
         <td><label for="password">Mot de passe:</label></td>
-        <td><input type="password" name="password" placeholder="Mot de passe" /></td>
+        <td><input type="password" name="password" placeholder="Mot de passe" required /></td>
     </tr>
     <tr>
         <td colspan="2"><input class="INPUT_submit" type="submit" value="Se connecter" /></td>
