16e26af855e11ea582abecd5fb3d394049f7ed69
Simon <simon.dujancourt.etu@univ-lemans.fr>
2013-06-29 13:48:17 +0200

    [Modèle] Ajout d'une méthode creerEtudiant

--- src/GestionNotes/Model/Etudiant.php
+++ src/GestionNotes/Model/Etudiant.php
@@ -2,6 +2,8 @@
 
 class GestionNotes_Model_Etudiant extends GestionNotes_Model_User
 {
+    // ------------------------- OPERATIONS DE LECTURE ------------------------- //
+    
     public static function fetchAll()
     {
     	$sth = self::$db->prepare('
@@ -87,4 +89,27 @@ class GestionNotes_Model_Etudiant extends GestionNotes_Model_User
                 $result[] = self::exchange($data);
         return $result;
     }
+    
+    // ------------------------- OPERATIONS DE MODIFICATION ------------------------- //
+    
+    public static function creerEtudiant(array $params)
+    {
+        $sth = self::$db->prepare('
+            INSERT INTO users (username, email, password, password_salt, type, first_name, last_name, apogee_code, formation_id)
+            VALUES (:username, :email, :password, :salt, '.self::TYPE_ETUDIANT.', :first_name, :last_name, :apogee, :formation)
+        ');
+        
+        $sth->bindParam(':username',    $params['apogee'], PDO::PARAM_STR);
+        $sth->bindParam(':email',       $params['email'], PDO::PARAM_STR);
+        $sth->bindParam(':salt',        $salt = uniqid(), PDO::PARAM_STR);
+        $sth->bindParam(':password',    md5(md5($params['password']).$salt), PDO::PARAM_STR);
+        $sth->bindParam(':first_name',  $params['nom'], PDO::PARAM_STR);
+        $sth->bindParam(':last_name',   $params['prenom'], PDO::PARAM_STR);
+        $sth->bindParam(':apogee',      $params['apogee'], PDO::PARAM_STR);
+        $sth->bindParam(':formation',   $params['formation'], PDO::PARAM_INT);
+        
+        return $sth->execute();
+    }
+    
+    // ------------------------- OPERATIONS DE SUPPRESSION ------------------------- //
 }
