dc5276917fea42c23028a0156349f8f9595b525c
Simon <simon.dujancourt.etu@univ-lemans.fr>
2013-07-01 09:07:14 +0200

    Refonte formulaire connexion

--- app/views/security/login.php
+++ app/views/security/login.php
@@ -1,31 +1,8 @@
-<form action="<?php echo $this->url('security/login') ?>" method="post">
-<table class="login-box">
-<tbody>
-    <tr>
-        <td colspan="2"><h2>Identification</h2></td>
-    </tr>
-    <?php if ( isset($errors) && count($errors) > 0 ): ?>
-    <tr class="errors">
-        <td colspan="2">
-            <ul>
-                <?php foreach ($errors as $error): ?>
-                <li><?php echo $error ?></li>
-                <?php endforeach ?>
-            </ul>
-        </td>
-    </tr>
-    <?php endif ?>
-    <tr>
-        <td><label for="username">Nom d'utilisateur:</label></td>
-        <td><input type="text" name="username" placeholder="Nom d'utilisateur" required /></td>
-    </tr>
-    <tr>
-        <td><label for="password">Mot de passe:</label></td>
-        <td><input type="password" name="password" placeholder="Mot de passe" required /></td>
-    </tr>
-    <tr>
-        <td colspan="2"><input class="INPUT_submit" type="submit" value="Se connecter" /></td>
-    </tr>
-</tbody>
-</table>
-</form>
+<div class="container">
+    <form action="<?php echo $this->url('security/login') ?>" class="form-signin" method="post">
+        <h2 class="form-signin-heading">Connexion</h2>
+        <input type="text" name="username" class="input-block-level" placeholder="Nom d'utilisateur" required />
+        <input type="password" name="password" class="input-block-level" placeholder="Mot de passe" required /></td>
+        <button class="btn btn-large btn-primary" type="submit">Se connecter</button>
+    </form>
+</div>
